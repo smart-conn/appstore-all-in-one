de9cd974e2e07d4a9e1252be58e091640a5d8cca
Gen Liu <liugen@smartconn.cc>
2016-04-18 14:15:18 +0000

    add user model passport converter
    Change-Id: Ieb9f134442537fb49467be32a015d044d16a93f3

--- models/user.js
+++ models/user.js
@@ -1,10 +1,16 @@
 "use strict";
 
 const Sequelize = require('sequelize');
+const passportLocalSequelize = require('passport-local-sequelize');
+
 module.exports = function (sequelize) {
 
-  sequelize.define('user', {
+  const User = sequelize.define('user', {
     name: Sequelize.STRING
+  });
+
+  passportLocalSequelize.attachToUser(User, {
+    usernameField: 'name'
   });
 
 };
